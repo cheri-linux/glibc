ee1c20316a13fbace4513d8b551e7ff08a237e96
Lukas Auer <lukas.auer@aisec.fraunhofer.de>
2021-03-29 21:32:05 +0200

    prctl: replace variadic function arguments using macros
    This is required due to CHERI's changes to the calling convention
    (passing arguments on the stack).

--- include/sys/prctl.h
+++ include/sys/prctl.h
@@ -3,7 +3,20 @@
 
 # ifndef _ISOMAC
 
+#ifndef __CHERI_PURE_CAPABILITY__
 extern int __prctl (int __option, ...);
+#else
+extern int __prctl (int __option, uintptr_t __arg2, uintptr_t __arg3,
+		    uintptr_t __arg4, uintptr_t __arg5);
+
+#define ___prctl_cheri0(option) (__prctl) (option, 0, 0, 0, 0)
+#define ___prctl_cheri1(option,a) (__prctl) (option, __prc(a), 0, 0, 0)
+#define ___prctl_cheri2(option,a,b) (__prctl) (option, __prc(a), __prc(b), 0, 0)
+#define ___prctl_cheri3(option,a,b,c) (__prctl) (option, __prc(a), __prc(b), __prc(c), 0)
+#define ___prctl_cheri4(option,a,b,c,d) (__prctl) (option, __prc(a), __prc(b), __prc(c), __prc(d))
+
+#define __prctl(...) __PRCTL_DISP (___prctl_cheri, __VA_ARGS__)
+#endif
 
 # endif /* !_ISOMAC */
 #endif
--- sysdeps/unix/sysv/linux/sys/prctl.h
+++ sysdeps/unix/sysv/linux/sys/prctl.h
@@ -20,11 +20,35 @@
 
 #include <features.h>
 #include <linux/prctl.h>  /*  The magic values come from here  */
+#include <stdint.h>
 
 __BEGIN_DECLS
 
 /* Control process execution.  */
+#ifndef __CHERI_PURE_CAPABILITY__
 extern int prctl (int __option, ...) __THROW;
+#else
+extern int prctl (int __option, uintptr_t __arg2, uintptr_t __arg3,
+		  uintptr_t __arg4, uintptr_t __arg5) __THROW;
+
+#ifndef __prc
+# define __prc(a) ((uintptr_t) (a))
+#endif
+
+#define __PRCTL_NARGS_X(a,b,c,d,e,f,...) f
+#define __PRCTL_NARGS(...) __PRCTL_NARGS_X (__VA_ARGS__,4,3,2,1,0,)
+#define __PRCTL_CONCAT_X(a,b) a##b
+#define __PRCTL_CONCAT(a,b) __PRCTL_CONCAT_X (a, b)
+#define __PRCTL_DISP(b,...) __PRCTL_CONCAT (b,__PRCTL_NARGS(__VA_ARGS__))(__VA_ARGS__)
+
+#define __prctl_cheri0(option) (prctl) (option, 0, 0, 0, 0)
+#define __prctl_cheri1(option,a) (prctl) (option, __prc(a), 0, 0, 0)
+#define __prctl_cheri2(option,a,b) (prctl) (option, __prc(a), __prc(b), 0, 0)
+#define __prctl_cheri3(option,a,b,c) (prctl) (option, __prc(a), __prc(b), __prc(c), 0)
+#define __prctl_cheri4(option,a,b,c,d) (prctl) (option, __prc(a), __prc(b), __prc(c), __prc(d))
+
+#define prctl(...) __PRCTL_DISP (__prctl_cheri, __VA_ARGS__)
+#endif
 
 __END_DECLS
 
