69c1dda159a7bea33b881ab39bd66d746772b3ef
Lukas Auer <lukas.auer@aisec.fraunhofer.de>
2021-03-30 12:47:09 +0200

    clone: replace variadic function arguments using macros
    This is required due to CHERI's changes to the calling convention
    (passing arguments on the stack).

--- include/sched.h
+++ include/sched.h
@@ -20,8 +20,27 @@ libc_hidden_proto (__sched_get_priority_min)
 extern int __sched_rr_get_interval (__pid_t __pid, struct timespec *__t);
 
 /* These are Linux specific.  */
+#ifndef __CHERI_PURE_CAPABILITY__
 extern int __clone (int (*__fn) (void *__arg), void *__child_stack,
 		    int __flags, void *__arg, ...);
+#else
+extern int __clone (int (*__fn) (void *__arg), void *__child_stack,
+		    int __flags, void *__arg, void *__parent_tidptr,
+		    void *__tls, void *__child_tidptr);
+
+#define __CLONE_NARGS_X(a,b,c,d,e,f,g,h,...) h
+#define __CLONE_NARGS(...) __CLONE_NARGS_X (__VA_ARGS__,3,2,1,0,)
+#define __CLONE_CONCAT_X(a,b) a##b
+#define __CLONE_CONCAT(a,b) __CLONE_CONCAT_X (a, b)
+#define __CLONE_DISP(b,...) __CLONE_CONCAT (b,__CLONE_NARGS(__VA_ARGS__))(__VA_ARGS__)
+
+#define __clone_cheri0(a,b,c,d) (__clone) (a, b, c, d, 0, 0, 0)
+#define __clone_cheri1(a,b,c,d,e) (__clone) (a, b, c, d, e, 0, 0)
+#define __clone_cheri2(a,b,c,d,e,f) (__clone) (a, b, c, d, e, f, 0)
+#define __clone_cheri3(a,b,c,d,e,f,g) (__clone) (a, b, c, d, e, f, g)
+
+#define __clone(...) __CLONE_DISP (__clone_cheri, __VA_ARGS__)
+#endif
 libc_hidden_proto (__clone)
 extern int __clone2 (int (*__fn) (void *__arg), void *__child_stack_base,
 		     size_t __child_stack_size, int __flags, void *__arg, ...);
