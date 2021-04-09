d781c6a6dd19442819f784fa87e9f2587554dffc
Julian Horsch <julian.horsch@aisec.fraunhofer.de>
2021-04-09 14:22:45 +0200

    prctl: add sixth argument, required e.g. by libcap

--- include/sys/prctl.h
+++ include/sys/prctl.h
@@ -7,13 +7,14 @@
 extern int __prctl (int __option, ...);
 #else
 extern int __prctl (int __option, uintptr_t __arg2, uintptr_t __arg3,
-		    uintptr_t __arg4, uintptr_t __arg5);
+		    uintptr_t __arg4, uintptr_t __arg5, uintptr_t __arg6);
 
-#define ___prctl_cheri0(option) (__prctl) (option, 0, 0, 0, 0)
-#define ___prctl_cheri1(option,a) (__prctl) (option, __prc(a), 0, 0, 0)
-#define ___prctl_cheri2(option,a,b) (__prctl) (option, __prc(a), __prc(b), 0, 0)
-#define ___prctl_cheri3(option,a,b,c) (__prctl) (option, __prc(a), __prc(b), __prc(c), 0)
-#define ___prctl_cheri4(option,a,b,c,d) (__prctl) (option, __prc(a), __prc(b), __prc(c), __prc(d))
+#define ___prctl_cheri0(option) (__prctl) (option, 0, 0, 0, 0, 0)
+#define ___prctl_cheri1(option,a) (__prctl) (option, __prc(a), 0, 0, 0, 0)
+#define ___prctl_cheri2(option,a,b) (__prctl) (option, __prc(a), __prc(b), 0, 0, 0)
+#define ___prctl_cheri3(option,a,b,c) (__prctl) (option, __prc(a), __prc(b), __prc(c), 0, 0)
+#define ___prctl_cheri4(option,a,b,c,d) (__prctl) (option, __prc(a), __prc(b), __prc(c), __prc(d), 0)
+#define ___prctl_cheri5(option,a,b,c,d,e) (__prctl) (option, __prc(a), __prc(b), __prc(c), __prc(d), __prc(e))
 
 #define __prctl(...) __PRCTL_DISP (___prctl_cheri, __VA_ARGS__)
 #endif
--- sysdeps/unix/sysv/linux/sys/prctl.h
+++ sysdeps/unix/sysv/linux/sys/prctl.h
@@ -29,23 +29,24 @@ __BEGIN_DECLS
 extern int prctl (int __option, ...) __THROW;
 #else
 extern int prctl (int __option, uintptr_t __arg2, uintptr_t __arg3,
-		  uintptr_t __arg4, uintptr_t __arg5) __THROW;
+		  uintptr_t __arg4, uintptr_t __arg5, uintptr_t __arg6) __THROW;
 
 #ifndef __prc
 # define __prc(a) ((uintptr_t) (a))
 #endif
 
-#define __PRCTL_NARGS_X(a,b,c,d,e,f,...) f
-#define __PRCTL_NARGS(...) __PRCTL_NARGS_X (__VA_ARGS__,4,3,2,1,0,)
+#define __PRCTL_NARGS_X(a,b,c,d,e,f,g,...) g
+#define __PRCTL_NARGS(...) __PRCTL_NARGS_X (__VA_ARGS__,5,4,3,2,1,0,)
 #define __PRCTL_CONCAT_X(a,b) a##b
 #define __PRCTL_CONCAT(a,b) __PRCTL_CONCAT_X (a, b)
 #define __PRCTL_DISP(b,...) __PRCTL_CONCAT (b,__PRCTL_NARGS(__VA_ARGS__))(__VA_ARGS__)
 
-#define __prctl_cheri0(option) (prctl) (option, 0, 0, 0, 0)
-#define __prctl_cheri1(option,a) (prctl) (option, __prc(a), 0, 0, 0)
-#define __prctl_cheri2(option,a,b) (prctl) (option, __prc(a), __prc(b), 0, 0)
-#define __prctl_cheri3(option,a,b,c) (prctl) (option, __prc(a), __prc(b), __prc(c), 0)
-#define __prctl_cheri4(option,a,b,c,d) (prctl) (option, __prc(a), __prc(b), __prc(c), __prc(d))
+#define __prctl_cheri0(option) (prctl) (option, 0, 0, 0, 0, 0)
+#define __prctl_cheri1(option,a) (prctl) (option, __prc(a), 0, 0, 0, 0)
+#define __prctl_cheri2(option,a,b) (prctl) (option, __prc(a), __prc(b), 0, 0, 0)
+#define __prctl_cheri3(option,a,b,c) (prctl) (option, __prc(a), __prc(b), __prc(c), 0, 0)
+#define __prctl_cheri4(option,a,b,c,d) (prctl) (option, __prc(a), __prc(b), __prc(c), __prc(d), 0)
+#define __prctl_cheri5(option,a,b,c,d,e) (prctl) (option, __prc(a), __prc(b), __prc(c), __prc(d), __prc(e))
 
 #define prctl(...) __PRCTL_DISP (__prctl_cheri, __VA_ARGS__)
 #endif
